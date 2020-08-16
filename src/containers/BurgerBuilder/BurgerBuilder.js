59b63d8b104772200a6f4750254ff26840c8caa6
Vishal Verma <vverma@expedien.in>
2020-08-16 21:07:56 +0530

    Adding Controls Files

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import CustomAux from '../../higher-order-component/CustomAux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
 
     render() {
         return (
             <CustomAux>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </CustomAux>
         );
     }
